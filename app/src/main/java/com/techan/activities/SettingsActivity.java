ac291c4b9376b55c1a5aa1969334aac956c8d72a
Aditya Kumar <aditya.kumar@gmail.com>
2014-04-26 19:07:46 -0500

    Cleaned up settings activity

--- app/src/main/java/com/techan/activities/SettingsActivity.java
+++ app/src/main/java/com/techan/activities/SettingsActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
-import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -21,8 +19,6 @@ public class SettingsActivity extends Activity {
     public static final String REFRESH_INTERVAL_KEY ="refreshInterval";
     public static final String REFRESH_WIFI_ONLY_KEY = "refreshWifiOnly";
     public static final String ALL_NOTIFICATIONS_KEY = "allNotifications";
-    public static final String PE_ENABLED_KEY ="peEnabled";
-    public static final String PE_TARGET_KEY = "peTarget";
     public static final int STOP_LOSS_DEFAULT = 25;
 
     @Override
@@ -52,20 +48,16 @@ public class SettingsActivity extends Activity {
 
             final SwitchPreference refreshPref = (SwitchPreference) getPreferenceManager().findPreference(AUTO_REFRESH_KEY);
             final ListPreference refreshPreferenceList = (ListPreference)getPreferenceManager().findPreference(REFRESH_INTERVAL_KEY);
-            final CheckBoxPreference refreshWifiOnlyCheckBox = (CheckBoxPreference)getPreferenceManager().findPreference(REFRESH_WIFI_ONLY_KEY);
-            handleRefreshes(getActivity().getApplicationContext(), refreshPref, refreshPreferenceList, refreshWifiOnlyCheckBox);
+            handleRefreshes(getActivity().getApplicationContext(), refreshPref, refreshPreferenceList);
 
             final SwitchPreference allNotificationsPref = (SwitchPreference)getPreferenceManager().findPreference(ALL_NOTIFICATIONS_KEY);
-            final SwitchPreference peSwitchPreference = (SwitchPreference)getPreferenceManager().findPreference(PE_ENABLED_KEY);
-            final EditTextPreference peEditPreference = (EditTextPreference)getPreferenceManager().findPreference(PE_TARGET_KEY);
 
-            handleAllNotifications(peSwitchPreference, peEditPreference, allNotificationsPref);
-            handlePeNotifications(peSwitchPreference, peEditPreference, allNotificationsPref);
+            handleAllNotifications(allNotificationsPref);
         }
     }
 
     private static void handleRefreshes(final Context appContext, final SwitchPreference refreshPref,
-                                        final ListPreference refreshPreferenceList, final CheckBoxPreference refreshWifiOnlyCheckBox) {
+                                        final ListPreference refreshPreferenceList) {
         refreshMessage(refreshPreferenceList.getValue(), refreshPreferenceList);
 
         refreshPreferenceList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
@@ -98,58 +90,10 @@ public class SettingsActivity extends Activity {
 
     }
 
-    private static void handleAllNotifications(final SwitchPreference peSwitchPreference, final EditTextPreference peEditPreference, final SwitchPreference allNotificationsPref) {
+    private static void handleAllNotifications(final SwitchPreference allNotificationsPref) {
         allNotificationsPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object o) {
-                boolean allNotState = (Boolean)o;
-
-                peSwitchPreference.setEnabled(allNotState);
-
-                // If switching off force pe switch to off.
-                // Must do this because if someone enabled a stock specific notification will
-                // enable "all notifications" but might not want the pe notification to activate
-                // as a result..... an example would be activating a stock specific stop loss notification
-                if(!allNotState)
-                    peSwitchPreference.setChecked(allNotState);
-
-                peEditPreference.setEnabled(allNotState);
-                return true;
-            }
-        });
-    }
-
-    private static void handlePeNotifications(final SwitchPreference peSwitchPreference, final EditTextPreference peEditPreference, final SwitchPreference allNotificationsPref) {
-
-        if(!allNotificationsPref.isChecked()) {
-            peSwitchPreference.setEnabled(false);
-            peEditPreference.setEnabled(false);
-        } else {
-            peEditPreference.setEnabled(peSwitchPreference.isChecked());
-        }
-        peEditPreference.setSummary("Global PE target set to " + peEditPreference.getText());
-
-        peSwitchPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-                peEditPreference.setEnabled((Boolean) o);
-                return true;
-            }
-        });
-
-        peEditPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-                peEditPreference.setSummary("Global PE target set to " + Double.parseDouble((String) o));
-                return true;
-            }
-        });
-
-        peEditPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference pref) {
-                EditTextPreference et = (EditTextPreference) pref;
-                et.getEditText().setSelection(et.getText().length());
                 return true;
             }
         });
